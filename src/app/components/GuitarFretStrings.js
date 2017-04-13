25898b664c2224186aa202080f49f3800845a663
Anatoli Makarevich <makaroni4@gmail.com>
2017-04-13 11:13:50 +0200

    Added strings

---
+++ src/app/components/GuitarFretStrings.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export class GuitarFretStrings extends React.Component {
+  render() {
+    return(
+      <div className="guitar-fret-strings">
+        <div className="guitar-fret-strings__string guitar-fret-strings__string--high-e-string">
+        </div>
+
+        <div className="guitar-fret-strings__string guitar-fret-strings__string--b-string">
+        </div>
+
+        <div className="guitar-fret-strings__string guitar-fret-strings__string--g-string">
+        </div>
+
+        <div className="guitar-fret-strings__string guitar-fret-strings__string--d-string">
+        </div>
+
+        <div className="guitar-fret-strings__string guitar-fret-strings__string--a-string">
+        </div>
+
+        <div className="guitar-fret-strings__string guitar-fret-strings__string--low-e-string">
+        </div>
+      </div>
+    );
+  }
+}
--- src/app/components/GuitarNeck.js
+++ src/app/components/GuitarNeck.js
@@ -1,43 +1,57 @@
 import React from "react";
 
+import { GuitarFretStrings } from "./GuitarFretStrings";
+
 export class GuitarNeck extends React.Component {
   render() {
     return(
       <div className="guitar-neck">
         <div className="guitar-neck__fret guitar-neck__fret--1">
+            <GuitarFretStrings />
         </div>
 
         <div className="guitar-neck__fret guitar-neck__fret--2">
+            <GuitarFretStrings />
         </div>
 
         <div className="guitar-neck__fret guitar-neck__fret--3 guitar-neck__fret--dotted">
+            <GuitarFretStrings />
         </div>
 
         <div className="guitar-neck__fret guitar-neck__fret--4">
+            <GuitarFretStrings />
         </div>
 
         <div className="guitar-neck__fret guitar-neck__fret--5 guitar-neck__fret--dotted">
+            <GuitarFretStrings />
         </div>
 
         <div className="guitar-neck__fret guitar-neck__fret--6">
+            <GuitarFretStrings />
         </div>
 
         <div className="guitar-neck__fret guitar-neck__fret--7 guitar-neck__fret--dotted">
+            <GuitarFretStrings />
         </div>
 
         <div className="guitar-neck__fret guitar-neck__fret--8">
+            <GuitarFretStrings />
         </div>
 
         <div className="guitar-neck__fret guitar-neck__fret--9 guitar-neck__fret--dotted">
+            <GuitarFretStrings />
         </div>
 
         <div className="guitar-neck__fret guitar-neck__fret--10">
+            <GuitarFretStrings />
         </div>
 
         <div className="guitar-neck__fret guitar-neck__fret--11">
+            <GuitarFretStrings />
         </div>
 
         <div className="guitar-neck__fret guitar-neck__fret--12 guitar-neck__fret--double-dotted">
+            <GuitarFretStrings />
         </div>
       </div>
     );
