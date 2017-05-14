cf5f710bca55445eb021c41dcf1306377c983279
Anatoli Makarevich <makaroni4@gmail.com>
2017-05-14 18:59:09 +0200

    Added AudioProcessor from GoogleChrome guitar tuner, TODO: strip out string detection and get actual frequency

--- src/app/components/Quiz.js
+++ src/app/components/Quiz.js
@@ -68,6 +68,307 @@ var pickRandom = function(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
+// https://github.com/GoogleChrome/guitar-tuner
+function AudioProcessor() {
+  this.FFTSIZE = 2048;
+  this.stream = null;
+  this.audioContext = new AudioContext();
+  this.analyser = this.audioContext.createAnalyser();
+  this.gainNode = this.audioContext.createGain();
+  this.microphone = null;
+
+  this.gainNode.gain.value = 0;
+  this.analyser.fftSize = this.FFTSIZE;
+  this.analyser.smoothingTimeConstant = 0;
+
+  this.frequencyBufferLength = this.FFTSIZE;
+  this.frequencyBuffer = new Float32Array(this.frequencyBufferLength);
+
+  this.sendingAudioData = false;
+
+  this.strings = {
+    e2: {
+      offset: Math.round(this.audioContext.sampleRate / 82.4069),
+      difference: 0
+    },
+
+    a2: {
+      offset: Math.round(this.audioContext.sampleRate / 110),
+      difference: 0
+    },
+
+    d3: {
+      offset: Math.round(this.audioContext.sampleRate / 146.832),
+      difference: 0
+    },
+
+    g3: {
+      offset: Math.round(this.audioContext.sampleRate / 195.998),
+      difference: 0
+    },
+
+    b3: {
+      offset: Math.round(this.audioContext.sampleRate / 246.932),
+      difference: 0
+    },
+
+    e4: {
+      offset: Math.round(this.audioContext.sampleRate / 329.628),
+      difference: 0
+    }
+  };
+
+  this.stringsKeys = Object.keys(this.strings);
+
+  this.lastRms = 0;
+  this.rmsThreshold = 0.006;
+  this.assessedStringsInLastFrame = false;
+  this.assessStringsUntilTime = 0;
+
+  this.requestUserMedia = function () {
+    var that = this;
+
+    navigator.getUserMedia({audio:true}, (stream) => {
+      that.sendingAudioData = true;
+      that.stream = stream;
+      that.microphone = that.audioContext.createMediaStreamSource(stream);
+      that.microphone.connect(that.analyser);
+      that.analyser.connect(that.gainNode);
+      that.gainNode.connect(that.audioContext.destination);
+
+      requestAnimationFrame(that.dispatchAudioData);
+
+    }, (err) => {
+      console.log('Unable to access the microphone');
+      console.log(err);
+    });
+  }
+
+  this.attached = function() {
+    // Set up the stream kill / setup code for visibility changes.
+    document.addEventListener('visibilitychange', this.onVisibilityChange);
+
+    // Then call it.
+    this.onVisibilityChange();
+  }
+
+  this.detached = function() {
+    this.sendingAudioData = false;
+  }
+
+  this.onVisibilityChange = function() {
+    console.log("--> onVisibilityChange")
+    if (document.hidden) {
+      this.sendingAudioData = false;
+
+      if (this.stream) {
+        // Chrome 47+
+        this.stream.getAudioTracks().forEach((track) => {
+          if ('stop' in track) {
+            track.stop();
+          }
+        });
+
+        // Chrome 46-
+        if ('stop' in this.stream) {
+          this.stream.stop();
+        }
+      }
+
+      this.stream = null;
+    } else {
+      this.requestUserMedia();
+    }
+
+  }
+
+  this.sortStringKeysByDifference = function(a, b) {
+    return this.strings[a].difference - this.strings[b].difference;
+  }
+
+  /**
+   * Autocorrelate the audio data, which is basically where you
+   * compare the audio buffer to itself, offsetting by one each
+   * time, up to the half way point. You sum the differences and
+   * you see how small the difference comes out.
+   */
+  this.autocorrelateAudioData = function(time) {
+
+    let searchSize = this.frequencyBufferLength * 0.5;
+    let sampleRate = this.audioContext.sampleRate;
+    let offsetKey = null;
+    let offset = 0;
+    let difference = 0;
+    let tolerance = 0.001;
+    let rms = 0;
+    let rmsMin = 0.008;
+    let assessedStringsInLastFrame = this.assessedStringsInLastFrame;
+
+    // Fill up the data.
+    this.analyser.getFloatTimeDomainData(this.frequencyBuffer);
+
+    // console.log(this.frequencyBuffer)
+    // var newBuff = [];
+    // for (let d = 0; d < this.frequencyBuffer.length; d++) {
+    //   newBuff[d] = Math.abs(this.frequencyBuffer[d]);
+    // }
+    // var maxIndex = newBuff.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+    // console.log(maxIndex)
+
+    // Figure out the root-mean-square, or rms, of the audio. Basically
+    // this seems to be the amount of signal in the buffer.
+    for (let d = 0; d < this.frequencyBuffer.length; d++) {
+      rms += this.frequencyBuffer[d] * this.frequencyBuffer[d];
+    }
+
+    rms = Math.sqrt(rms / this.frequencyBuffer.length);
+
+    // If there's little signal in the buffer quit out.
+    if (rms < rmsMin)
+      return 0;
+
+    // Only check for a new string if the volume goes up. Otherwise assume
+    // that the string is the same as the last frame.
+    if (rms > this.lastRms + this.rmsThreshold)
+      this.assessStringsUntilTime = time + 250;
+
+    if (time < this.assessStringsUntilTime) {
+
+      this.assessedStringsInLastFrame = true;
+
+      // Go through each string and figure out which is the most
+      // likely candidate for the string being tuned based on the
+      // difference to the "perfect" tuning.
+      for (let o = 0; o < this.stringsKeys.length; o++) {
+
+        offsetKey = this.stringsKeys[o];
+        offset = this.strings[offsetKey].offset;
+        difference = 0;
+
+        // Reset how often this string came out as the closest.
+        if (assessedStringsInLastFrame === false)
+          this.strings[offsetKey].difference = 0;
+
+        // Now we know where the peak is, we can start
+        // assessing this sample based on that. We will
+        // step through for this string comparing it to a
+        // "perfect wave" for this string.
+        for (let i = 0; i < searchSize; i++) {
+          difference += Math.abs(this.frequencyBuffer[i] -
+              this.frequencyBuffer[i + offset]);
+        }
+
+        difference /= searchSize;
+
+        // Weight the difference by frequency. So lower strings get
+        // less preferential treatment (higher offset values). This
+        // is because harmonics can mess things up nicely, so we
+        // course correct a little bit here.
+        this.strings[offsetKey].difference += (difference * offset);
+      }
+
+    } else {
+      this.assessedStringsInLastFrame = false;
+    }
+
+    // If this is the first frame where we've not had to reassess strings
+    // then we will order by the string with the largest number of matches.
+    if (assessedStringsInLastFrame === true &&
+        this.assessedStringsInLastFrame === false) {
+      this.stringsKeys.sort(this.sortStringKeysByDifference);
+    }
+
+    // Next for the top candidate in the set, figure out what
+    // the actual offset is from the intended target.
+    // We'll do it by making a full sweep from offset - 10 -> offset + 10
+    // and seeing exactly how long it takes for this wave to repeat itself.
+    // And that will be our *actual* frequency.
+    let searchRange = 10;
+    let assumedString = this.strings[this.stringsKeys[0]];
+    let searchStart = assumedString.offset - searchRange;
+    let searchEnd = assumedString.offset + searchRange;
+    let actualFrequency = assumedString.offset;
+    let smallestDifference = Number.POSITIVE_INFINITY;
+
+    for (let s = searchStart; s < searchEnd; s++) {
+
+      difference = 0;
+
+      // For each iteration calculate the difference of every element of the
+      // array. The data in the buffer should be PCM, so values ranging
+      // from -1 to 1. If they match perfectly then they'd essentially
+      // cancel out. But this is real data so we'll be looking for small
+      // amounts. If it's below tolerance assume a perfect match, otherwise
+      // go with the smallest.
+      //
+      // A better version of this would be to curve match on the data.
+      for (let i = 0; i < searchSize; i++) {
+        difference += Math.abs(this.frequencyBuffer[i] -
+            this.frequencyBuffer[i + s]);
+      }
+
+      difference /= searchSize;
+
+      if (difference < smallestDifference) {
+        smallestDifference = difference;
+        actualFrequency = s;
+      }
+
+      if (difference < tolerance) {
+        actualFrequency = s;
+        break;
+      }
+    }
+
+    this.lastRms = rms;
+
+    return this.audioContext.sampleRate / actualFrequency;
+
+  }
+
+  this.dispatchAudioData = function(time) {
+
+    // Always set up the next pass here, because we could
+    // early return from this pass if there's not a lot
+    // of exciting data to deal with.
+    if (this.sendingAudioData)
+      requestAnimationFrame(this.dispatchAudioData);
+
+    let frequency = this.autocorrelateAudioData(time);
+
+    if (frequency === 0)
+      return;
+
+    // Convert the most active frequency to linear, based on A440.
+    let dominantFrequency = Math.log2(frequency / 440);
+
+    // Figure out how many semitones that equates to.
+    let semitonesFromA4 = 12 * dominantFrequency;
+
+    // The octave is A440 for 4, so start there, then adjust by the
+    // number of semitones. Since we're at A, we need only 3 more to
+    // push us up to octave 5, and 9 to drop us to 3. So there's the magic
+    // 9 in that line below accounted for.
+    let octave = 4 + ((9 + semitonesFromA4) / 12);
+    octave = Math.floor(octave);
+
+    // The note is 0 for A, all the way to 11 for G#.
+    let note = (12 + (Math.round(semitonesFromA4) % 12)) % 12;
+
+    // Now tell anyone who's interested.
+    let notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+    console.log([frequency, octave, notes[note]])
+    // YOUR CODE GOES HERE
+  }
+
+  // Bind as we would have done for anything in the constructor so we can use
+  // them without confusing what 'this' means. Yay window scoped.
+  this.dispatchAudioData = this.dispatchAudioData.bind(this);
+  this.sortStringKeysByDifference = this.sortStringKeysByDifference.bind(this);
+  this.onVisibilityChange = this.onVisibilityChange.bind(this);
+}
+
+
 export class Quiz extends React.Component {
   constructor() {
     super();
@@ -92,6 +393,11 @@ export class Quiz extends React.Component {
       prevNote: this.state.currentNote,
       prevAnswer: answerCorrect
     });
+  }
+
+  componentDidMount() {
+    var processor = new AudioProcessor();
+    processor.attached();
   }
 
   handleAnswer(string, fret) {
